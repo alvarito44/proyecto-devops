e1c8255ad4c965575d023915a9aea3f6aa730fc9
alvarito44 <alvarojorquera44@gmail.com>
2024-05-10 10:39:21 -0400

    add an exception if the api_jey is not found

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import { dividir, multiplicar, restar, sumar } from "./calcular.js";
 
 const app = express();
 const ambiente = process.env.NODE_ENV || 'desconocido'
-const mi_api_key = fs.readFileSync('./api_key.txt', 'utf8');
+const mi_api_key = fs.readFileSync('./api_key.txt', 'utf8') || 'te falta el api_key';
 
 app.use(express.json());
 
@@ -31,7 +31,7 @@ app.get("/ambiente", (req,res) => {
 });
 
 app.get("/api", (req,res) => {
-  res.send(`La llave es:${mi_api_key}` );
+  res.send(`La llave es: ${mi_api_key}` );
 });
 
 export default app;
